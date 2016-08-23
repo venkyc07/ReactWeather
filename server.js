c1d30f37de1740e1ea8e30e7d6ea03c60bb42c87
penguin <venky@penguincrm.com>
2016-08-23 15:40:43 +0530

    prep repo for Heroku

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ var express = require('express');
 
 //Create our app
 var app = express();
+cpnst PORT = procee.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  if(req.headers['x-forwardedproto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
-app.listen (3000, function () {
-  console.log('Express  server is up on port 3000');
+app.listen (PORT, function () {
+  console.log('Express  server is up on port 3000' + PORT);
 });
